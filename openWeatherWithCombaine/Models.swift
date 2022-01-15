7f919980bdd0f26097a0d4022ec2dd58067d1039
Nikolay Vinogradov <nikolya0209@mail.ru>
2022-01-15 19:45:11 +0300

    init

--- openWeatherWithCombaine/Models.swift
+++ openWeatherWithCombaine/Models.swift
@@ -6,3 +6,71 @@
 //
 
 import Foundation
+
+struct WeatherData: Codable {
+    let coord: Coord?
+    let weather: [Weather]?
+    let base: String?
+    let main: Main?
+    let visibility: Int?
+    let wind: Wind?
+    let clouds: Clouds?
+    let dt: Int?
+    let sys: Sys?
+    let id: Int?
+    let name: String?
+    let cod: Int?
+    
+    static var placeHolder: Self {
+        return WeatherData(coord: nil, weather: nil, base: nil, main: nil, visibility: nil, wind: nil, clouds: nil, dt: nil, sys: nil, id: nil, name: nil, cod: nil)
+    }
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+}
--- openWeatherWithCombaine/ViewController.swift
+++ openWeatherWithCombaine/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import Combine
+
+enum WeatherError: Error {
+    case invalidResponse
+}
 
 class ViewController: UIViewController {
+    
+    
+    private let celsiusCharacters = "°С"
+    private let openWeatherBaseURL = "https://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherAPIKey = "2fde583d9bff183c3d658143a82b5cfb"
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    private var cancelLabel: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicatorView.isHidden = true
     }
 
-
+    @IBAction func searchTap(_ sender: Any) {
+        view.endEditing(true)
+        
+        guard let cityName = cityTextField.text else { return }
+        getTemperature(for: cityName)
+    }
+    
+    
+    private func getTemperature(for cityName: String) {
+        guard let weatherURL = URL(string: "\(openWeatherBaseURL)?q=\(cityName)&appid=\(openWeatherAPIKey)&units=metric") else { return }
+        activityIndicatorView.startAnimating()
+        searchButton.isEnabled = false
+        
+        cancelLabel = URLSession.shared.dataTaskPublisher(for: weatherURL)
+            .tryMap { data, response -> Data in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw WeatherError.invalidResponse
+                }
+                return data
+            }
+            .decode(type: WeatherData.self, decoder: JSONDecoder())
+            .catch({ error in
+                return Just(WeatherData.placeHolder)
+            })
+            .map({ $0.main?.temp ?? 0.0 })
+            .map({ "\($0) \(self.celsiusCharacters)"})
+            .subscribe(on: DispatchQueue(label: "Combine.Weather"))
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { completion in
+                self.activityIndicatorView.stopAnimating()
+                self.searchButton.isEnabled = true
+            }, receiveValue: { temp in
+                self.temperatureLabel.text = "Temperature is: \(temp)"
+            })
+    }
+    
 }
 
